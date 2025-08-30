1f454d3a562ce193f2f42db96eaafde23964dee9
Jay Dixit <jaydixit.work@gmail.com>
2025-08-30 16:12:43 -0600

    debug: identify gallery image import data flow issue
    - Gallery handler receiving nil for image-imports
    - Source file paths not being updated
    - Duplicate image processing in ox-astro.el and filter causing conflicts
    - Need to consolidate image processing in one place

--- debug.el
+++ debug.el
@@ -4,4 +4,8 @@ org-mode source file: /Users/jay/Library/CloudStorage/Dropbox/github/ox-astro/te
 
 [ox-astro][img] collected=(/Users/jay/Library/CloudStorage/Dropbox/github/astro-monorepo/apps/jaydocs/src/assets/images/posts/masimo-dutti.webp /Users/jay/Downloads/imag106910784_3615490375147223_8376969077115059792n.jpg /Users/jay/Downloads/imag50296363324533456719590617_6999624331092055355n.jpg /Users/jay/Downloads/Rachide-News-01(1).jpg /Users/jay/Downloads/2017021468194489.webp)
 [ox-astro][img] Processing GALLERY block
+[ox-astro][img] GALLERY image-imports: nil
+[ox-astro][img] GALLERY checking link path: /Users/jay/Downloads/imag50296363324533456719590617_6999624331092055355n.jpg (sanitized: imag50296363324533456719590617-6999624331092055355n.jpg), found in imports: NO
+[ox-astro][img] GALLERY checking link path: /Users/jay/Downloads/Rachide-News-01(1).jpg (sanitized: Rachide-News-01-1-.jpg), found in imports: NO
+[ox-astro][img] GALLERY checking link path: /Users/jay/Downloads/2017021468194489.webp (sanitized: 2017021468194489.webp), found in imports: NO
 [ox-astro][img] GALLERY generating component with 0 images
